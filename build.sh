adda2ebad2f09f0daf65133acadf9ceb0a0a1c83
harumu <rushia0901@gmail.com>
2025-04-06 18:23:24 +0900

    lk

--- build.sh
+++ build.sh
@@ -14,3 +14,25 @@ apt-get update && apt-get install -y google-chrome-stable
 
 # Pythonパッケージインストール
 pip install -r requirements.txt
+
+# # Google Chrome のインストール
+# # まず Chocolatey をインストールします (Chocolatey は Windows 用のパッケージマネージャーです)
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# # Chocolatey を使って Google Chrome をインストール
+# choco install googlechrome -y
+
+# # ChromeDriver のインストール
+# # Google Chrome のバージョンを確認して、それに対応する ChromeDriver をダウンロードします
+# # 例: ChromeDriver バージョン 134 の場合
+# $chromeDriverVersion = '134.0.6998.165' # このバージョンは Google Chrome のバージョンに合わせて変更してください
+# Invoke-WebRequest "https://chromedriver.storage.googleapis.com/134.0.6998.165/chromedriver_win32.zip" -OutFile 'C:\Users\harumu.DESKTOP-J6AHOEG\OneDrive\kadai-inform\myproject\chromedriver.exe'
+# # Expand-Archive "chromedriver.zip" -DestinationPath "C:\path\to\your\project"
+
+# # Python パッケージインストール
+# # 仮想環境を作成して、requirements.txt を使って依存パッケージをインストール
+# python -m venv venv
+# .\venv\Scripts\activate
+
+# # 依存パッケージのインストール
+# pip install -r requirements.txt
