ffa872f71770cea0474b002b52c01303eb0ced34
harumu <rushia0901@gmail.com>
2025-04-12 13:27:42 +0900

    ok

--- core/views.py
+++ core/views.py
@@ -9,7 +9,8 @@ class HomeView(TemplateView):
     template_name = "core/home.html"
 
     def post(self, *args, **kwargs):
-        inform_date_data = scraping()
-        day = inform_date_data[2]
-        auto_email(day)
+        # inform_date_data = scraping()
+        # day = inform_date_data[2]
+        # auto_email(day)
+        auto_email()
         return render(self.request, self.template_name)
