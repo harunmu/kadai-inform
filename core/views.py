e2931da7dec20b664a319938d4ef9931272f253e
harumu <rushia0901@gmail.com>
2025-04-12 22:55:24 +0900

    ok

--- core/views.py
+++ core/views.py
@@ -4,14 +4,24 @@ from django.views.generic import TemplateView, View
 # Create your views here.
 from main import scraping
 from craft_mail import auto_email
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 class HomeView(TemplateView):
     template_name = "core/home.html"
 
-    def post(self, *args, **kwargs):
-        inform_date_data = scraping()
-        # day = inform_date_data[2]
-        # auto_email(day)
-        # day = '123'
-        auto_email(inform_date_data)
-        return render(self.request, self.template_name)
+    # def post(self, *args, **kwargs):
+    #     inform_date_data = scraping()
+    #     # day = inform_date_data[2]
+    #     # auto_email(day)
+    #     # day = '123'
+    #     auto_email(inform_date_data)
+    #     return render(self.request, self.template_name)
+    
+    @csrf_exempt
+    def cron_run(request):
+        if request == 'GET':
+            inform_date_data = scraping()
+            auto_email(inform_date_data)
+            return JsonResponse({'status':'success'})
+        return JsonResponse({'status':'invalid method'},status=405)
