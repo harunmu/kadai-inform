0eec89bb31e94e77f39f6f64d9fc3bf3b2a6eccf
HARUMU <rushia0901@gmail.com>
2025-05-09 11:57:03 +0900

    y

--- myproject/settings.py
+++ myproject/settings.py
@@ -96,7 +96,11 @@ import os
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
