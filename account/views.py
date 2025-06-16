03a1543a66d48301bd1c725a8c2d9dbd2a2f783a
harumu <rushia0901@gmail.com>
2025-06-16 14:37:29 +0900

    add login_info

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from django.views.generic import CreateView
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
+from create_message import auto_email
 from .forms import SignupForm
 
 User = get_user_model()
@@ -13,6 +14,17 @@ class SignupView(CreateView):
     model = User
     form_class = SignupForm
     success_url = reverse_lazy('account:signup_done')
+
+    def form_valid(self,form):
+        response = super().form_valid(form)
+        user = self.object
+
+        title = "Thank You for Registering with service"
+        email_address = [user.email]
+        message = "登録が完了しました。"
+        auto_email(title,email_address,message)
+
+        return response
 
 class SignupDoneView(TemplateView):
     template_name = "registration/signup_done.html"
---
+++ create_message.py
@@ -0,0 +1,35 @@
+import datetime
+from email.mime.text import MIMEText
+import smtplib
+
+def auto_email(title,email_address,message):
+
+    # メール情報の設定
+
+    from_email = 'rushia0901@gmail.com'
+    to_email = email_address
+    mail_title = title
+    message = message
+
+    # MIMEオブジェクトでメールを作成
+
+    msg = MIMEText(message, 'plain')
+    # 第一引数に本文、第二引数にメール形式　(HTML形式の場合 'html')
+    msg['Subject'] = mail_title #件名
+    msg['To'] = to_email #送信元
+    msg['From'] = from_email #宛先
+
+    # サーバー指定
+
+    smtp_host = 'smtp.gmail.com'
+    smtp_port = '587'
+    smtp_password = 'igtyubqrehudffyt'
+    server = smtplib.SMTP(smtp_host,smtp_port)
+    # 第一引数でサーバーアドレス、第二引数でポート番号
+    server.starttls() #TLSモードでSMPT接続
+    server.login(from_email,smtp_password) #暗号化した通信でgmailのsmtpサーバーにログイン
+    server.send_message(msg) #メール送信
+    server.quit() #接続を終了 
+
+if __name__ == '__main__':
+    auto_email()
